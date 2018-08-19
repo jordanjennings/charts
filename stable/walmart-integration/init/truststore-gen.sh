9ff7463a45d024c2cf12bece32b9529565f46f6f
Marc Sensenich <marc.sensenich@bossanova.com>
2018-08-19 09:57:38 -0400

    [INF-205] Use Script to Manage Keystore on Init
    Attempts to delete the WM aliases before adding them once more to the keystore

---
+++ stable/walmart-integration/init/truststore-gen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apk add --no-cache openssl coreutils
+
+echo |  openssl s_client -showcerts -servername $WM_API_ENDPOINT -connect $WM_API_ENDPOINT:$WM_API_PORT 2> /dev/null | sed -n "/^-----BEGIN CERT/,/^-----END CERT/p" > $WM_CERTS_TMP 
+csplit -z -f wm-crt- $WM_CERTS_TMP "/-----BEGIN CERTIFICATE-----/" "{*}"
+for file in wm-crt-*; do 
+    # INF-205: Attempt to delete the aliased certificate in the case it already exists
+    keytool -delete -alias walmart-external-api-$file -keystore $TRUSTSTORE_JKS -storepass $TRUSTSTORE_SECRET || true
+    keytool -import -noprompt -file $file -keystore $TRUSTSTORE_JKS -storepass $TRUSTSTORE_SECRET -alias walmart-external-api-$file
+done
+
+keytool -importkeystore -srckeystore /opt/jdk/jre/lib/security/cacerts -destkeystore $TRUSTSTORE_JKS -srcstorepass changeit -deststorepass $TRUSTSTORE_SECRET
